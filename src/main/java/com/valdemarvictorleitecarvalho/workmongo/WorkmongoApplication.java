d36e971afdf9dc55de83272bf462e7d3eee23e24
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-01-13 15:00:37 -0300

    Project created

---
+++ src/main/java/com/valdemarvictorleitecarvalho/workmongo/WorkmongoApplication.java
@@ -0,0 +1,13 @@
+package com.valdemarvictorleitecarvalho.workmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdemarvictorleitecarvalho/workmongo/WorkmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdemarvictorleitecarvalho.workmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
