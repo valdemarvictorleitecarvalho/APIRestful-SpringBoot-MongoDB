9f9aeeb005f67bf16329630a94bd2c0ae1bd62bd
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-01-13 17:58:42 -0300

    Getting a user by id

--- src/main/java/com/valdemarvictorleitecarvalho/workmongo/services/UserService.java
+++ src/main/java/com/valdemarvictorleitecarvalho/workmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.valdemarvictorleitecarvalho.workmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdemarvictorleitecarvalho.workmongo.domain.User;
 import com.valdemarvictorleitecarvalho.workmongo.repository.UserRepository;
+import com.valdemarvictorleitecarvalho.workmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
---
+++ src/main/java/com/valdemarvictorleitecarvalho/workmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.valdemarvictorleitecarvalho.workmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
