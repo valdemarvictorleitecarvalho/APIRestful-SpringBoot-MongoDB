801c49d6a0da4be708b9df96a42f65c958be6f46
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-01-14 11:45:09 -0300

    Endpoint to return a user posts

--- src/main/java/com/valdemarvictorleitecarvalho/workmongo/resources/UserResource.java
+++ src/main/java/com/valdemarvictorleitecarvalho/workmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.valdemarvictorleitecarvalho.workmongo.domain.Post;
 import com.valdemarvictorleitecarvalho.workmongo.domain.User;
 import com.valdemarvictorleitecarvalho.workmongo.dto.UserDTO;
 import com.valdemarvictorleitecarvalho.workmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
